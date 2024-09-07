d9ed974aa8b498ea089c633f9bd123ab2d15136e
Korkin Alexsandr <korkin.06@inbox.ru>
2024-09-07 19:42:50 +0300

    Заменил магические сеттеры и геттеры на обычные

--- 3 kurs/pract_1/app/Book.php
+++ 3 kurs/pract_1/app/Book.php
@@ -1,99 +1,99 @@
 <?php
 
-class Book {
+class Book
+{
     private string $title;
     private string $author;
     private int $ublishedYear;
     private string $genre;
 
-    public function __get($property)
+    public function getTitle()
     {
-        switch ($property)
-        {
-            case "title":
-                return $this->title;
-            case "author":
-                return $this->author;
-            case "ublishedYear":
-                return $this->ublishedYear;
-            case "genre":
-                return $this->genre;
-            default:
-                throw new ValueError("Данный атрибут не найден.");
+        return $this->title;
+    }
+
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    public function getUblishedYear()
+    {
+        return $this->ublishedYear;
+    }
+
+    public function getGenre()
+    {
+        return $this->genre;
+    }
+
+    public function setTitle(string $title)
+    {
+        if (!is_string($title)) {
+            throw new TypeError("Атрибут должен быть строкой.");
         }
-    }
 
-    public function __set($name, $value) {
-
-        switch ($name) {
-            
-            case "title":
-
-                if (!is_string($value)) {
-                    throw new TypeError("Атрибут должен быть строкой.");
-                }
-
-                if (empty($value)) {
-                    throw new ValueError("Атрибут 'name' не должен быть пустым.");
-                }
-
-                $this->title = $value;
-
-                break;
-
-            case "author":
-                if (is_string($value)) {
-                    throw new TypeError("Атрибут должен быть строкой.");
-                }
-
-                if (empty($value)) {
-                    throw new ValueError("Атрибут 'author' не должен быть пустым.");
-                }
-                
-                $this->author = $value;
-                break;
-
-            case "ublishedYear":
-                if (is_integer($value)) {
-                    throw new TypeError("Атрибут должен быть целым числом.");
-                }
-
-                if (empty($value)) {
-                    throw new ValueError("Атрибут 'ublishedYear' не должен быть пустым.");
-                }
-
-                if ($value <= 0) {
-                    throw new ValueError("Значение атрибута не должно быть меньше или равно нулю.");
-                }
-                
-                $this->ublishedYear = $value;
-                break;
-            
-            case "genre":
-                if (is_string($value)) {
-                    throw new TypeError("Атрибут должен быть строкой.");
-                }
-
-                if (empty($value)) {
-                    throw new ValueError("Атрибут 'genre' не должен быть пустым.");
-                }
-                
-                $this->genre = $value;
-                break;
-
-            default:
-                throw new ValueError("Данный атрибут не найден.");
+        if (empty($title)) {
+            throw new ValueError("Атрибут 'name' не должен быть пустым.");
         }
+
+        $this->title = $title;
     }
 
-    public function __construct($title, $author, $ublishedYear, $genre) {
-        $this->__set("title", $title);
-        $this->__set("author", $author);
-        $this->__set("ublishedYear", $ublishedYear);
-        $this->__set("genre", $genre);
+    public function setAuthor(string $author)
+    {
+        if (is_string($author)) {
+            throw new TypeError("Атрибут должен быть строкой.");
+        }
+
+        if (empty($value)) {
+            throw new ValueError("Атрибут 'author' не должен быть пустым.");
+        }
+
+        $this->author = $author;
     }
 
-    public function getBookInfo() {
+    public function setUblishedYear(int $ublishedYear)
+    {
+        if (is_integer($ublishedYear)) {
+            throw new TypeError("Атрибут должен быть целым числом.");
+        }
+
+        if (empty($ublishedYear)) {
+            throw new ValueError("Атрибут 'ublishedYear' не должен быть пустым.");
+        }
+
+        if ($ublishedYear <= 0) {
+            throw new ValueError("Значение атрибута не должно быть меньше или равно нулю.");
+        }
+
+        $this->ublishedYear = $ublishedYear;
+    }
+
+    public function setGenre(string $genre)
+    {
+        if (is_string($genre)) {
+            throw new TypeError("Атрибут должен быть строкой.");
+        }
+
+        if (empty($genre)) {
+            throw new ValueError("Атрибут 'genre' не должен быть пустым.");
+        }
+
+        $this->genre = $genre;
+    }
+
+
+    public function __construct($title, $author, $ublishedYear, $genre)
+    {
+        $this->setTitle($title);
+        $this->setAuthor($author);
+        $this->setUblishedYear($ublishedYear);
+        $this->setGenre($genre);
+    }
+
+    public function getBookInfo()
+    {
         return "$this->title, $this->author, $this->ublishedYear, $this->ublishedYear";
     }
 }
