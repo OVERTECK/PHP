c7de6a01ad57fe6bff54766199be0480b55910ce
Korkin Alexsandr <korkin.06@inbox.ru>
2024-09-29 15:04:50 +0300

    Поменял название директории

---
+++ kurs_3/pract_exceptions/index.php
@@ -0,0 +1,22 @@
+<?php
+
+$x = 100;
+
+class User
+{
+    public string $name;
+
+    function test()
+    {
+        echo "123";
+    }
+}
+
+function number()
+{
+    foreach ($variable as $key => $value) {
+        # code...
+    }
+}
+
+number();
