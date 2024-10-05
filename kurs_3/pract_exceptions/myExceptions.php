4275f00653598f06f275c00501728bf776e72065
Korkin Alexsandr <korkin.06@inbox.ru>
2024-10-05 10:52:22 +0300

    Убрал исключение IncorrectPaymentMethod, добавил InsufficientFundsException, PaymentGatewayException

--- kurs_3/pract_exceptions/myExceptions.php
+++ kurs_3/pract_exceptions/myExceptions.php
@@ -5,3 +5,7 @@ namespace myExceptions;
 class OutOfStockException extends \Exception {}
 
 class CartLimitExceededException extends \Exception {}
+
+class InsufficientFundsException extends \Exception {}
+
+class PaymentGatewayException extends \Exception {}
