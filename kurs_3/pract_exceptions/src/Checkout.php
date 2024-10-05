9099e33dc62bbfd4d6eeef1e2ae5e0096393fe27
Korkin Alexsandr <korkin.06@inbox.ru>
2024-10-05 22:41:53 +0300

    Изменение директории

---
+++ kurs_3/pract_exceptions/src/Checkout.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Checkout;
+
+require_once 'vendor\\autoload.php';
+
+use Cart\Cart;
+use myExceptions\CartLimitExceededException;
+use myExceptions\InsufficientFundsException;
+use myExceptions\OutOfStockException;
+use myExceptions\PaymentGatewayException;
+
+class Checkout
+{
+    private Cart $cart;
+    private string $paymentMethod = 'credit card';
+    private float $userBalance = 200;
+
+    public function __construct(Cart $cart)
+    {
+        $this->cart = $cart;
+    }
+
+    public function setPaymentMethod($method): void
+    {
+        $this->paymentMethod = $method;
+    }
+
+    /**
+     * Summary of processPayment
+     * @param mixed $amount
+     * @throws \myExceptions\PaymentGatewayException
+     * @throws \myExceptions\InsufficientFundsException
+     * @return void
+     */
+    public function processPayment($amount): void
+    {
+        $method = $this->paymentMethod;
+
+        switch ($method) {
+            case 'credit card':
+                break;
+            default:
+                throw new PaymentGatewayException('Данный способ оплаты не найден.');
+        }
+
+        if ($amount > $this->userBalance) {
+            throw new InsufficientFundsException("Баланс меньше общей стоимости оплаты.");
+        }
+    }
+
+    public function finalizeOrder(): void
+    {
+        try {
+            $this->processPayment($this->cart->getTotal());
+        } catch (InsufficientFundsException | PaymentGatewayException $ex) {
+            echo $ex->getMessage();
+        } catch (CartLimitExceededException | OutOfStockException $ex) {
+            echo $ex->getMessage();
+        }
+    }
+}
