bf5e54a3a367d5c7024c31aff77afc80ebc9352d
Korkin Alexsandr <korkin.06@inbox.ru>
2024-10-05 22:38:53 +0300

    Изменил подключение файлов на autoload

--- kurs_3/pract_exceptions/index.php
+++ kurs_3/pract_exceptions/index.php
@@ -2,9 +2,7 @@
 
 declare(strict_types=1);
 
-require_once "Cart.php";
-require_once "Product.php";
-require_once "Checkout.php";
+require_once 'vendor\\autoload.php';
 
 use Cart\Cart;
 use Product\Product;
@@ -14,18 +12,12 @@ $apple = new Product('apple', 20.5, 100);
 $banana = new Product('banana', 30, 200);
 $milk = new Product('milk', 65, 150);
 
-try {
-    $cart = new Cart();
+$cart = new Cart();
 
-    $cart->addItem($apple, 1);
-    $cart->addItem($banana, 3);
-    $cart->addItem($milk, 3);
+$cart->addItem($apple, 1);
+$cart->addItem($banana, 3);
+$cart->addItem($milk, 3);
 
-    $checkout = new Checkout($cart);
+$checkout = new Checkout($cart);
 
-    $checkout->finalizeOrder();
-} catch (myExceptions\OutOfStockException $ex) {
-    echo $ex->getMessage();
-} catch (myExceptions\CartLimitExceededException $ex) {
-    echo $ex->getMessage();
-}
+$checkout->finalizeOrder();
