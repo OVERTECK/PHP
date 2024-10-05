f3dabc00d488d6ab597a9f7d01d774e9c87879d3
Korkin Alexsandr <korkin.06@inbox.ru>
2024-10-05 10:47:15 +0300

    Добавление класса Checkout

---
+++ kurs_3/pract_exceptions/Checkout.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Checkout;
+
+use Cart\Cart;
+use myExceptions\IncorrectPaymentMethod;
+use myExceptions\InsufficientFundsException;
+use myExceptions\PaymentGatewayException;
+
+class Checkout
+{
+    private Cart $cart;
+    private string $paymentMethod = 'credit card';
+    private float $userBalance;
+
+    public function __construct(Cart $cart)
+    {
+        $this->cart = $cart;
+    }
+
+    public function setPaymentMethod($method): void
+    {
+        $this->paymentMethod = $method;
+    }
+
+    public function processPayment($amount): void
+    {
+        $method = $this->paymentMethod;
+
+        switch ($method) {
+            case 'credit card':
+                break;
+            default:
+                throw new IncorrectPaymentMethod('Данный способ оплаты не найден.');
+        }
+
+        if ($amount > $this->userBalance) {
+            throw new InsufficientFundsException("Баланс меньше общей стоимости оплаты.");
+        }
+    }
+
+    public function finalizeOrder(): void
+    {
+        try {
+            $this->processPayment($this->cart->getTotal());
+        } catch (InsufficientFundsException $ex) {
+            echo $ex->getMessage();
+        } catch (PaymentGatewayException $ex) {
+            echo $ex->getMessage();
+        }
+    }
+}
