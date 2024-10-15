55d1577f8cdcd4fb0d00d755f3605c02e1c9c16e
Korkin Alexsandr <korkin.06@inbox.ru>
2024-10-15 12:02:42 +0300

    Добавил отправку сообщения через EmailNotification

--- kurs_3/pract_3/src/index.php
+++ kurs_3/pract_3/src/index.php
@@ -9,11 +9,17 @@ require_once "vendor\\autoload.php";
 
 $notManager = new NotificationManager();
 
-$smsNot = new SMSNotification("Отправлено", "Обычное");
+$smsNot = new SMSNotification("Отправлено", "SMS");
+
+$emailNot = new EmailNotification("Отправлено", "Email");
+
 
 $notManager->sendNotification($smsNot, "Привет");
 
 $notManager->sendNotification($smsNot, "Some message");
+
+$notManager->sendNotification($emailNot, "Email message");
+
 
 print_r($notManager->getNotificationHistory());
 
