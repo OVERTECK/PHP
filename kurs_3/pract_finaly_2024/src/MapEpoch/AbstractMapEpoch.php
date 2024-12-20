13047526e43ef7d96ef46ec33e8a1182b6fc7d38
Korkin Alexsandr <korkin.06@inbox.ru>
2024-12-20 13:00:04 +0300

    Added file

---
+++ kurs_3/pract_finaly_2024/src/MapEpoch/AbstractMapEpoch.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace src;
+
+abstract class AbstractMapEpoch
+{
+    protected string $color;
+
+    protected int $price;
+
+    protected string $effect;
+
+    protected string $title;
+
+    protected string $charColorBlind;
+
+    protected string $numberEpoch;
+
+    protected int $minCountPlayers;
+}
---
+++ kurs_3/pract_finaly_2024/src/MapEpoch/BrownCart.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace src;
+
+class BrownCart extends AbstractMapEpoch
+{
+    protected string $color = "brown";
+
+
+}
