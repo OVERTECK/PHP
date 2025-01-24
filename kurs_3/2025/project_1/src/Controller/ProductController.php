1ce1d0268b598873b59f3d2a02edcff2f1fded8f
Korkin Alexsandr <korkin.06@inbox.ru>
2025-01-24 19:21:31 +0300

    Added CRUD user and product

--- kurs_3/2025/project_1/src/Controller/ProductController.php
+++ kurs_3/2025/project_1/src/Controller/ProductController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use App\Entity\Product;
 use App\Repository\ProductRepository;
-use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,53 +12,65 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class ProductController extends AbstractController
 {
-    #[Route('/product', name: 'app_product')]
+    #[Route('/product', name: 'app_product', methods:["GET"])]
     public function index(ProductRepository $productRepository): Response
     {
         $products = $productRepository->findAll();
 
-        return $this->render('product/index.html.twig',
-        ["products" => $products]);
+        return $this->render(
+            'product/index.html.twig',
+            ['products' => $products]
+        );
     }
 
-    #[Route('/product/create', name: 'create_product_page', methods:["GET"])]
+    #[Route('/product/create', name: 'create_product_page', methods:['GET'])]
     public function createProductPage(): Response
     {
-        return $this->render("product/createProduct.html.twig");
+        return $this->render('product/createProduct.html.twig');
     }
 
-    #[Route('/product/create', name: 'create_product', methods: ["POST"])]
+    #[Route('/product/create', name: 'create_product', methods: ['POST'])]
     public function createProduct(EntityManagerInterface $em, Request $request): Response
     {
         $product = new Product();
 
-        $product->setName($request->request->get("name"));
-        $product->setCount($request->request->get("count"));
-        $product->setPrice($request->request->get("price"));
+        $product->setName($request->request->get('name'));
+        $product->setCount($request->request->get('count'));
+        $product->setPrice($request->request->get('price'));
 
         $em->persist($product);
 
         $em->flush();
 
-        return $this->redirect("/product");
+        return $this->redirect('/product');
     }
 
-    #[Route('/product/edit/{product}', name: 'edit_page', methods: ["GET"])]
-    public function editPage(Product $product): Response
+    #[Route('/product/edit/{product}', name: 'edit_page_product', methods: ['POST'])]
+    public function editPageProduct(Product $product): Response
     {
-        return $this->render("product/edit.html.twig",
-        ["product" => $product]);
+        return $this->render('product/edit.html.twig',
+        ['product' => $product]);
     }
 
-    #[Route('/product/edit/{product}', name: 'edit', methods: ["PUT"])]
+    #[Route('/product/edit/{product}', name: 'edit_product', methods: ['PUT'])]
     public function edit(Product $product, Request $request, EntityManagerInterface $em): Response
     {
-        $product->setName($request->request->get("name"));
-        $product->setCount($request->request->get("count"));
-        $product->setPrice($request->request->get("price"));
+        $product->setName($request->request->get('name'));
+        $product->setCount($request->request->get('count'));
+        $product->setPrice($request->request->get('price'));
 
         $em->flush();
 
-        return $this->redirect("/product");
+        return $this->redirect('/product');
+    }
+
+    #[Route('/product/delete/{product}', name: 'delete_product', methods: ["DELETE"])]
+    public function deleteProduct(Product $product, EntityManagerInterface $em): Response
+    {
+        $em->remove($product);
+
+        $em->flush();
+
+        return $this->redirect('/product');
     }
 }
--- kurs_3/2025/project_1/src/Controller/UserController.php
+++ kurs_3/2025/project_1/src/Controller/UserController.php
@@ -2,17 +2,83 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\User;
 
 final class UserController extends AbstractController
 {
-    #[Route('/user', name: 'app_user')]
-    public function index(): Response
+    #[Route('/user', name: 'users', methods:["GET"])]
+    public function users(UserRepository $userRepository): Response
     {
-        return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
-        ]);
+        $users = $userRepository->findAll();
+
+        return $this->render(
+            'user/readUser.html.twig',
+            ['users' => $users]
+        );
+    }
+
+    #[Route('/user/create', name: 'create_user_page', methods: ['GET'])]
+    public function createProductPage(): Response
+    {
+        return $this->render('user/createUser.html.twig');
+    }
+
+    #[Route('/user/create', name: 'create_user', methods: ['POST'])]
+    public function createUser(EntityManagerInterface $em, Request $request): Response
+    {
+        $user = new User();
+
+        $user->setFirstName($request->request->get('first_name'));
+        $user->setSecondName($request->request->get('second_name'));
+        $user->setLastName($request->request->get('last_name'));
+        $user->setAddress($request->request->get('address'));
+        $user->setPhone($request->request->get('phone'));
+        $user->setEmail($request->request->get('email'));
+
+        $em->persist($user);
+
+        $em->flush();
+
+        return $this->redirect('/user');
+    }
+
+    #[Route('/user/edit/{user}', name: 'edit_page', methods: ['POST'])]
+    public function editPage(User $user): Response
+    {
+        return $this->render(
+            'user/editUser.html.twig',
+            ['user' => $user]
+        );
+    }
+
+    #[Route('/user/edit/{user}', name: 'edit', methods: ['PUT'])]
+    public function edit(User $user, Request $request, EntityManagerInterface $em): Response
+    {
+        $user->setFirstName($request->request->get('first_name'));
+        $user->setLastName($request->request->get('last_name'));
+        $user->setSecondName($request->request->get('second_name'));
+        $user->setAddress($request->request->get('address'));
+        $user->setPhone($request->request->get('phone'));
+        $user->setEmail($request->request->get('email'));
+
+        $em->flush();
+
+        return $this->redirect('/user');
+    }
+
+    #[Route('/user/delete/{user}', name: 'delete_user', methods: ["DELETE"])]
+    public function deleteUser(User $user, EntityManagerInterface $em): Response
+    {
+        $em->remove($user);
+
+        $em->flush();
+
+        return $this->redirect('/user');
     }
 }
